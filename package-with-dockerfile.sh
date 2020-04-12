594e8849c52f9119a2644f5134973e55fd366fa9
Christian Bremer <bremersee@googlemail.com>
2020-04-12 16:49:36 +0200

    Clean gh-pages

--- package-with-dockerfile.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-VERSION="$1"
-if [ -z "$VERSION" ]; then
-    mvn -Ddockerfile.skip=false clean package
-else
-    mvn -Ddockerfile.skip=false -Ddockerfile.tag="$VERSION" clean package
-fi
